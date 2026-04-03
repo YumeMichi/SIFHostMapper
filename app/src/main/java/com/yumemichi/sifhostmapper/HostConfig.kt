f9568ec4ce428f94debf27fb6aff5361d42329f5
Sean Du <do4suki@gmail.com>
2026-04-03 20:23:13 +0800

    Remove unused hosts
    No longer needed. https://github.com/YumeMichi/llsif-cn-client/commit/5f0dfc455b22dfe5b78d5dc64df41bbe92d3bf6d
    
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- app/src/main/java/com/yumemichi/sifhostmapper/HostConfig.kt
+++ app/src/main/java/com/yumemichi/sifhostmapper/HostConfig.kt
@@ -2,11 +2,7 @@ package com.yumemichi.sifhostmapper
 
 object HostConfig {
     val DEFAULT_HOSTS = listOf(
-        "adv.sdo.com",
         "mgame.sdo.com",
-        "reportsk-nh.sdo.com",
-        "reportsk.web.sdo.com",
-        "utility.sdoprofile.com",
         "prod.game1.ll.sdo.com"
     )
 }

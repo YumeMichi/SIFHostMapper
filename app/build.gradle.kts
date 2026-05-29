690642d5b584d1e7373d9f6c0e2ca6627932bbf9
Sean Du <do4suki@gmail.com>
2026-05-29 11:21:22 +0800

    Bump app version to 1.2
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yumemichi.sifhostmapper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "1.2"
     }
 
     buildTypes {
